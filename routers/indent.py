2b049a7f7d77f9c00477cf4c12a21502abf808af
Pankaj8700 <pankajku.8700@gmail.com>
2025-04-11 11:50:38 +0530

    change in qr generation code

--- routers/indent.py
+++ routers/indent.py
@@ -10,35 +10,43 @@ router = APIRouter()
 
 ## indent issue route
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @router.post("/create_indent_for_Non_Consumable", tags=["Indent"])
 def create_indent(indent: IndentCreate, session: Session = Depends(get_session)):
-    indent_model = Indent(
-        item_name=indent.item_name,
-        Quantity=indent.Quantity,
-        Department=indent.Department,
-        Item_Type="Non Consumable"
-    )
-    session.add(indent_model)
-    session.commit()
-    session.refresh(indent_model)
-
-    # Generate QR code
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=5,
-        border=4, 
+    try:
+        indent_model = Indent(
+            item_name=indent.item_name,
+            Quantity=indent.Quantity,
+            Department=indent.Department,
+            Item_Type="Non Consumable"
         )
-    qr.add_data(f"Indent ID: {indent_model.indent_id},Date of Indent: {indent_model.date_of_indent},Item Name: {indent_model.item_name}, Quantity: {indent_model.Quantity}, Department: {indent_model.Department}, Item Type: {indent_model.Item_Type}")
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
+        session.add(indent_model)
+        session.commit()
+        session.refresh(indent_model)
 
-    # Save the QR code image to a BytesIO object
-    img_io = BytesIO()
-    img.save(img_io, 'PNG')
-    img_io.seek(0)
+        # Generate QR code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=5,
+            border=4, 
+        )
+        qr.add_data(f"Indent ID: {indent_model.indent_id},Date of Indent: {indent_model.date_of_indent},Item Name: {indent_model.item_name}, Quantity: {indent_model.Quantity}, Department: {indent_model.Department}, Item Type: {indent_model.Item_Type}")
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
 
-    return StreamingResponse(img_io, media_type="image/png")
+        # Save the QR code image to a BytesIO object
+        img_io = BytesIO()
+        img.save(img_io, 'PNG')
+        img_io.seek(0)
+
+        return StreamingResponse(img_io, media_type="image/png")
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        raise
 
 # indent issue for consumable items
 @router.post("/create_indent_for_Consumable",response_model=IndentResponse, tags=["Indent"])
