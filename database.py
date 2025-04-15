3af53befc21200b23e08f18547373f2827aa15b2
Pankaj8700 <pankajku.8700@gmail.com>
2025-04-15 16:29:05 +0530

    done addition of postgresql

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from sqlmodel import create_engine, SQLModel, Session
-from urllib.parse import quote_plus
+# from urllib.parse import quote_plus
+from dotenv import load_dotenv
+import os
 
 # MySQL database configuration
 # mysql_username = "root"
@@ -14,9 +16,12 @@ from urllib.parse import quote_plus
 # # MySQL connection URL
 # DB_url = f"mysql+pymysql://{mysql_username}:{encoded_password}@{mysql_host}:{mysql_port}/{mysql_database}"
 # engine = create_engine(DB_url, echo=True)
-sqlite_file_name = "database.db"  # SQLite database file
-sqlite_url = f"sqlite:///{sqlite_file_name}"  # SQLite connection URL
-engine = create_engine(sqlite_url, echo=True)  
+# sqlite_file_name = "database.db"  # SQLite database file
+# sqlite_url = f"sqlite:///{sqlite_file_name}"  # SQLite connection URL
+
+# PostgreSQL database configuration
+DATABASE_URL = os.getenv("DB_URL") 
+engine = create_engine(DATABASE_URL, echo=True)  
 
 # Function to create database tables
 def create_db_and_tables():
