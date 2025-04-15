e181788511f752e509151eb91ec50dc2f20fc9e3
Pankaj8700 <pankajku.8700@gmail.com>
2025-04-15 16:03:15 +0530

    sqlite setup done

--- database.py
+++ database.py
@@ -2,21 +2,21 @@ from sqlmodel import create_engine, SQLModel, Session
 from urllib.parse import quote_plus
 
 # MySQL database configuration
-mysql_username = "root"
-mysql_password = "42xeZalB@"
-mysql_host = "localhost"
-mysql_port = 3306
-mysql_database = "inventory_management"
+# mysql_username = "root"
+# mysql_password = "42xeZalB@"
+# mysql_host = "localhost"
+# mysql_port = 3306
+# mysql_database = "inventory_management"
 
-# URL-encode the password
-encoded_password = quote_plus(mysql_password)
+# # URL-encode the password
+# encoded_password = quote_plus(mysql_password)
 
 # # MySQL connection URL
-DB_url = f"mysql+pymysql://{mysql_username}:{encoded_password}@{mysql_host}:{mysql_port}/{mysql_database}"
-engine = create_engine(DB_url, echo=True)
-# sqlite_file_name = "database.db"  # SQLite database file
-# sqlite_url = f"sqlite:///{sqlite_file_name}"  # SQLite connection URL
-# engine = create_engine(sqlite_url, echo=True)  
+# DB_url = f"mysql+pymysql://{mysql_username}:{encoded_password}@{mysql_host}:{mysql_port}/{mysql_database}"
+# engine = create_engine(DB_url, echo=True)
+sqlite_file_name = "database.db"  # SQLite database file
+sqlite_url = f"sqlite:///{sqlite_file_name}"  # SQLite connection URL
+engine = create_engine(sqlite_url, echo=True)  
 
 # Function to create database tables
 def create_db_and_tables():
